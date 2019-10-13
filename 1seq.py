b559c9bf6d3719818ca6d550dba4334ef89b6261
DONaumenko <dmitry.naumenko@gmail.com>
2019-10-14 01:50:51 +0300

    dz

---
+++ 1seq.py
@@ -0,0 +1,9 @@
+quantity_of_numbers = int(input('Введите количество элементов списка'))
+numbers = []
+
+for i in range(0, quantity_of_numbers):
+    number = int(input(f'Введите {i+1} элемент'))
+    numbers.append(number)
+
+numbers.sort()
+print(numbers)
